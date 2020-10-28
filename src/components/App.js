48be8f426c5a343308c2f362e4dbd5bb2b74c043
YuTung Cheng <ychevng55@myseneca.ca>
2020-10-27 20:47:53 -0400

    Forth Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
-import useVideos from '../hooks/useVideos';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos('buildings');
+  const [videos, search] = useVideos("ReactJS");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
